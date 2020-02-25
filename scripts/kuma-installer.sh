365ee9ac64149a0e45a7adad5f3fe8becab5ab4d
Beau Frusetta <beau@frusetta.com>
2020-02-25 12:54:31 -0700

    Updates to the Kuma installer
    Trimmed down the Kuma installer script since it was quite repetitive. I
    ended up putting all of the dataplane configurations in their own files
    under the /config/dataplanes folder. The new installer now goes through
    a list of services, and loads the appropriate dataplane to register.

--- scripts/kuma-installer.sh
+++ scripts/kuma-installer.sh
@@ -17,7 +17,6 @@ function fail {
 
 function create_dataplane {
   DATAPLANE_HOSTNAME="$1"
-  DATAPLANE_RESOURCE="$2"
   DATAPLANE_NAME="${DATAPLANE_HOSTNAME}"
 
   #
@@ -29,6 +28,8 @@ function create_dataplane {
     fail "failed to resolve IP address allocated to the '${DATAPLANE_HOSTNAME}' container"
   fi
   echo "'${DATAPLANE_HOSTNAME}' has the following IP address: ${DATAPLANE_IP_ADDRESS}"
+
+  DATAPLANE_RESOURCE=$(cat /kuma/dataplanes/${DATAPLANE_NAME}.yml | sed -e "s/IP/${DATAPLANE_IP_ADDRESS}/g")
 
   #
   # Create Dataplane for "${DATAPLANE_NAME}"
@@ -57,131 +58,13 @@ KUMA_CONTROL_PLANE_URL=http://kuma-control-plane:5681
 kumactl config control-planes add --name universal --address ${KUMA_CONTROL_PLANE_URL} --admin-client-cert /certs/client/cert.pem --admin-client-key /certs/client/cert.key --overwrite
 
 #
-# Create Dataplane for `edgex-core-consul` service
+# Create Dataplane for all services
 #
+DATAPLANES="edgex-core-consul edgex-redis edgex-support-logging edgex-sys-mgmt-agent  \
+            edgex-support-notifications edgex-core-metadata edgex-core-data edgex-core-command \
+            edgex-support-scheduler edgex-app-service-configurable-rules edgex-support-rulesengine \
+            edgex-device-virtual"
 
-create_dataplane "edgex-core-consul" "
-type: Dataplane
-mesh: default
-name: edgex-core-consul
-networking:
-  inbound:
-    - interface: {{ IP }}:58400:8400
-      tags:
-        service: edgex-core-consul
-    - interface: {{ IP }}:58500:8500
-      tags:
-        service: edgex-core-consul"
-
-create_dataplane "edgex-redis" "
-type: Dataplane
-mesh: default
-name: edgex-redis
-networking:
-  inbound:
-    - interface: {{ IP }}:56379:6379
-      tags:
-        service: edgex-redis"
-
-create_dataplane "edgex-support-logging" "
-type: Dataplane
-mesh: default
-name: edgex-support-logging
-networking:
-  inbound:
-    - interface: {{ IP }}:58061:48061
-      tags:
-        service: edgex-support-logging"
-
-create_dataplane "edgex-sys-mgmt-agent" "
-type: Dataplane
-mesh: default
-name: edgex-sys-mgmt-agent
-networking:
-  inbound:
-    - interface: {{ IP }}:58090:48090
-      tags:
-        service: edgex-sys-mgmt-agent"
-
-create_dataplane "edgex-support-notifications" "
-type: Dataplane
-mesh: default
-name: edgex-support-notifications
-networking:
-  inbound:
-    - interface: {{ IP }}:58060:48060
-      tags:
-        service: edgex-support-notifications"
-
-create_dataplane "edgex-core-metadata" "
-type: Dataplane
-mesh: default
-name: edgex-core-metadata
-networking:
-  inbound:
-    - interface: {{ IP }}:58081:48081
-      tags:
-        service: edgex-core-metadata"
-
-create_dataplane "edgex-core-data" "
-type: Dataplane
-mesh: default
-name: edgex-core-data
-networking:
-  inbound:
-    - interface: {{ IP }}:58080:48080
-      tags:
-        service: edgex-core-data
-    - interface: {{ IP }}:55563:5563
-      tags:
-        service: edgex-core-data"
-
-create_dataplane "edgex-core-command" "
-type: Dataplane
-mesh: default
-name: edgex-core-command
-networking:
-  inbound:
-    - interface: {{ IP }}:58082:48082
-      tags:
-        service: edgex-core-command"
-
-create_dataplane "edgex-support-scheduler" "
-type: Dataplane
-mesh: default
-name: edgex-support-scheduler
-networking:
-  inbound:
-    - interface: {{ IP }}:58085:48085
-      tags:
-        service: edgex-support-scheduler"
-
-create_dataplane "edgex-app-service-configurable-rules" "
-type: Dataplane
-mesh: default
-name: edgex-app-service-configurable-rules
-networking:
-  inbound:
-    - interface: {{ IP }}:58100:48100
-      tags:
-        service: edgex-app-service-configurable-rules"
-
-create_dataplane "edgex-support-rulesengine" "
-type: Dataplane
-mesh: default
-name: edgex-support-rulesengine
-networking:
-  inbound:
-    - interface: {{ IP }}:58075:48075
-      tags:
-        service: edgex-support-rulesengine"
-
-create_dataplane "edgex-device-virtual" "
-type: Dataplane
-mesh: default
-name: edgex-device-virtual
-networking:
-  inbound:
-    - interface: {{ IP }}:59990:49990
-      tags:
-        service: edgex-device-virtual"
+for DP in ${DATAPLANES}; do 
+  create_dataplane ${DP}
+done
